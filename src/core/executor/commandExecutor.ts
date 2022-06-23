52f223f656370abe14245ad3659eecc31705b930
dmitry.sokolov <dmitry.sokolov@jetrockets.ru>
2022-06-23 10:13:57 +0300

    Added CommandExecutor

---
+++ src/core/executor/commandExecutor.ts
@@ -0,0 +1,20 @@
+import { ChildProcessWithoutNullStreams } from "child_process";
+
+import { IStreamLogger } from "../handlers/streamLogger.interface";
+import { ICommandExec } from './command.types';
+
+export abstract class CommandExecutor<T> {
+  constructor(private logger: IStreamLogger) {}
+
+  public async execute() {
+    const input = await this.prompt();
+    const command = this.build(input);
+    const stream = this.spawn(command);
+    this.processStream(stream, this.logger)
+  }
+
+  protected abstract prompt(): Promise<T>
+  protected abstract build(input: T): ICommandExec
+  protected abstract spawn(command: ICommandExec): ChildProcessWithoutNullStreams
+  protected abstract processStream(stream: ChildProcessWithoutNullStreams, logger: IStreamLogger): void
+}
